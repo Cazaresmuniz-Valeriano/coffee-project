09e05e7bf5954094f28ceccbcd0cd9cee9de98b6
Sergio Cazaresmuniz <cazasergio96@gmail.com>
2021-01-28 11:38:01 -0600

    feat: added pictures

--- main.js
+++ main.js
@@ -1,21 +1,34 @@
 "use strict"
 
 function renderCoffee(coffee) {
-    var html = '<div class="col md-4">';
-    // html += '<div class= "card p-2">';
-    // html += '<div class= "row no-gutters">';
-    // html += '<div class= "col">';
-    // html += '<div>';
-    // html += '<div class= "col-md-8">';
-    // html += '<div class= "card-body p-0 text-center h-100 d-flex justify-content-center flex-column">';
-    html += '<h4 class="headings">' + coffee.name + ' </h4> ';
+
+    var cardPicture = '';
+    function pickPic(object){
+        if (object.roast === "light") {
+            cardPicture = "img/light.jpg"
+        } else if (object.roast === "medium") {
+            cardPicture = "img/medium.jpg"
+        } else {
+            cardPicture = "img/dark.jpg"
+        }
+        return cardPicture;
+    }
+
+    var html = '<div class="col mb-4">';
+    html += '<div class="card p-2">';
+    html += '<div class="row no-gutters">';
+    html += '<div class="col">';
+    html += '<img src="' + pickPic(coffee) + '" class="card-img" alt="coffee">';
+    html += '</div>'
+    html += '<div class="col-md-8">'
+    html += '<div class="card-body p-0 text-center h-100 d-flex justify-content-center flex-column">';
+    html += '<h4 class="headings">' + coffee.name + '</h4>';
     html += '<p class="sizing">' + coffee.roast + '</p>';
-    // html += '<div>';
-    // html += '<div>';
-    // html += '<div>';
-    // html += '<div>';
-    // html += '<div>';
-    // html += '<div>';
+    html += '</div>';
+    html += '</div>';
+    html += '</div>';
+    html += '</div>';
+    html += '</div>';
 
 
     return html;
